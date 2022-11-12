82c910f17c0f9718f504e1051bcce7243a444345
Julian Ruestig <jruestig@posteo.de>
2022-11-12 05:36:25 +0100

    Gaussian fit, random position, sis deflection

--- gaussian_fit.py
+++ gaussian_fit.py
@@ -6,18 +6,17 @@ import matplotlib.pyplot as plt
 
 x = jnp.linspace(-10, 10.1, 100)
 x = np.meshgrid(x, x)
-x = x[0]**2 + x[1]**2
+y = x[0]**2 + x[1]**2
 
-pi_true = jnp.array((0.2, 1.0, 3.3))
+pi_true = jnp.array((0.2, 1.0, 3.3, 1.2, 3.2))
 
 
-# def f(c):
-#     a, b = c
-#     return x
+def f(d, e):
+    return (x[0]-d)**2 + (x[1]-e)**2
 
 def modelnew(pi):
-    a, b, c = pi
-    return a * jnp.exp(-0.5*b*(x-c)**2)
+    a, b, c, d, e = pi
+    return a * jnp.exp(-0.5*b*(f(d, e)-c)**2)
 
 def loss(pi, d):
     return jnp.mean((d - modelnew(pi))**2)
@@ -27,8 +26,8 @@ def update(pi, d, lr=1e-3):
     return pi - lr * jax.grad(loss)(pi, d)
 
 
-pi = jnp.array((1., 1., 1.))
-dnew = modelnew(pi_true) + np.random.normal(scale=0.001, size=x.shape)
+pi = jnp.array((1., 1., 1., 1., 1.))
+dnew = modelnew(pi_true) + np.random.normal(scale=0.001, size=y.shape)
 
 fig, axes = plt.subplots(1, 3)
 axes[0].imshow(dnew)
@@ -36,9 +35,8 @@ axes[1].imshow(modelnew(pi))
 axes[2].imshow(dnew-modelnew(pi))
 plt.show()
 
-pi = jnp.array((1., 1., 1.))
-for _ in range(40000):
-    pi = update(pi, dnew, 1.15)
+for _ in range(50000):
+    pi = update(pi, dnew, 2.15)
     print(pi)
 
 
