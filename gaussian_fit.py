460354873a9e9ac2c05f5aa34bb7bc7c38073430
Julian Ruestig <jruestig@posteo.de>
2022-11-12 05:08:41 +0100

    2d gaussian fit

--- gaussian_fit.py
+++ gaussian_fit.py
@@ -4,15 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = jnp.linspace(0, 10.1, 100)
+x = jnp.linspace(-10, 10.1, 100)
+x = np.meshgrid(x, x)
+x = x[0]**2 + x[1]**2
 
-pi_true = jnp.array((0.2, 1.0, 0.3))
+pi_true = jnp.array((0.2, 1.0, 3.3))
 
 
+# def f(c):
+#     a, b = c
+#     return x
 
 def modelnew(pi):
     a, b, c = pi
-    return c * jnp.exp(-0.5*a*(x-b)**2)
+    return a * jnp.exp(-0.5*b*(x-c)**2)
 
 def loss(pi, d):
     return jnp.mean((d - modelnew(pi))**2)
@@ -21,20 +26,24 @@ def loss(pi, d):
 def update(pi, d, lr=1e-3):
     return pi - lr * jax.grad(loss)(pi, d)
 
-pi = jnp.array((1., 1., 1.))
-dnew = modelnew(pi_true)
 
-plt.scatter(x, dnew)
-plt.plot(x, modelnew(pi))
+pi = jnp.array((1., 1., 1.))
+dnew = modelnew(pi_true) + np.random.normal(scale=0.001, size=x.shape)
+
+fig, axes = plt.subplots(1, 3)
+axes[0].imshow(dnew)
+axes[1].imshow(modelnew(pi))
+axes[2].imshow(dnew-modelnew(pi))
 plt.show()
 
-
-for _ in range(2000):
-    pi = update(pi, dnew, 1.)
+pi = jnp.array((1., 1., 1.))
+for _ in range(40000):
+    pi = update(pi, dnew, 1.15)
     print(pi)
 
 
-
-plt.scatter(x, dnew)
-plt.plot(x, modelnew(pi))
+fig, axes = plt.subplots(1, 3)
+axes[0].imshow(dnew)
+axes[1].imshow(modelnew(pi))
+axes[2].imshow(dnew-modelnew(pi))
 plt.show()
