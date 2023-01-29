3ef6fb9b2715c4b6529b93fc910f2e34b88edc3b
Julian Ruestig <jruestig@posteo.de>
2023-01-29 22:08:39 +0100

    Before new Interpolator

--- comparetoSimona.py
+++ comparetoSimona.py
@@ -8,7 +8,7 @@ import jax.numpy as jnp
 from jax import jit
 
 from source_fwd import (
-    load_fits, Blurring, lens_to_params)
+    save_fits, load_fits, Blurring, lens_to_params)
 from image_positions.image_positions import Interpolator
 from scipy.interpolate import RectBivariateSpline
 from os.path import join, exists
@@ -74,9 +74,11 @@ with open(cfg_file, 'r') as file:
 cluster_sources = np.load(cfg['general']['sources'], allow_pickle=True).item()
 clusterlist = list(cluster_sources.keys()); clusterlist.sort()
 
-cluster = 'bacco_1'
+ii = int(sys.argv[-1])
+
+cluster = 'bacco_3'
 sources = cluster_sources[cluster]
-source = sources[3]
+source = sources[ii]
 
 
 detectorspace = cf.Space(
@@ -135,21 +137,23 @@ else:
     alpha = model.deflection_point(
         detectorspace.xycoords[:, xslice, yslice], position)
 beta = np.array(detectorspace.xycoords[:, xslice, yslice] - alpha)
+# beta = beta[:, ~np.any(beta>50, axis=0)]
+# beta = beta[:, ~np.any(beta<-50, axis=0)]
 # beta[0] = beta[0] - source['position'][0]
 # beta[1] = beta[1] - source['position'][1]
-y = beta
 # y = beta[:, source['mask'][xslice, yslice]]
+y = beta.reshape(2, -1)
 
 extremum = np.max((np.abs(y.min()), np.abs(y.max()))) + resolution
 sidelength = 2 * extremum
 pixels = int(np.ceil(sidelength/resolution))
 print(pixels)
+y = np.array((y[0]-extremum, y[1]-extremum))  # /(sspace.extent[1]*2)
 
 # Defining domains & operators
 sspace = cf.Space((pixels,)*2, resolution)
 isspace = ift.RGSpace((pixels,)*2, resolution)
-y = np.array((y[0]-extremum, y[1]-extremum))  # /(sspace.extent[1]*2)
-interpolator = ift.LinearInterpolator(isspace, array(y.reshape(2, -1)))
+interpolator = ift.LinearInterpolator(isspace, array(y))
 Trans = Transponator(isspace)
 
 
@@ -182,7 +186,8 @@ data = ift.makeField(
 
 
 psf = load_fits(cfg['detector']['path_to_psf'])[:-1, :]
-B = lambda x: Blurring(x, psf[11:20, 11:20])
+# B = lambda x: Blurring(x, psf[11:20, 11:20])
+B = lambda x: Blurring(x, psf)
 BB = ift.JaxLinearOperator(dspace, dspace, B, domain_dtype=float)
 Re = Reshaper(interpolator.target, dspace)
 data = ift.makeField(dspace, d)
@@ -192,7 +197,7 @@ def power_spectrum(k):
     return 1000/(0.01+k)**3
 
 
-imargs = {'extent': detectorspace.extent, 'origin': 'lower'}
+imargs = {'extent': (xmin, xmax, ymin, ymax), 'origin': 'lower'}
 harmonic_space = isspace.get_default_codomain()
 HT = ift.HarmonicTransformOperator(harmonic_space, target=isspace)
 power_space = ift.PowerSpace(harmonic_space)
@@ -204,10 +209,10 @@ R = BB @ Re @ interpolator @ Trans @ HT
 
 print('start rec')
 data_space = R.target
-N = ift.ScalingOperator(data_space, 1/noise_scale)
+N = ift.ScalingOperator(data_space, noise_scale**2, sampling_dtype=float)
 D_inv = R.adjoint @ N.inverse @ R + S.inverse
 j = R.adjoint_times(N.inverse_times(data))
-IC = ift.GradientNormController(iteration_limit=500, tol_abs_gradnorm=1e-3)
+IC = ift.GradientNormController(iteration_limit=600, tol_abs_gradnorm=1e-3)
 D = ift.InversionEnabler(D_inv, IC, approximation=S.inverse).inverse
 m = D(j)
 
@@ -216,15 +221,16 @@ field = B(field.reshape(d.shape))
 
 
 source_reconstruction = HT(m).val
-maxi = 240
+maxi = 120
 maxind = np.unravel_index(
     np.argmax(source_reconstruction, axis=None),
     source_reconstruction.shape
 )
 
 slicer = slice(maxi//2, (maxi*3)//2)
-source_real = np.zeros((maxi*2,)*2)
-source_real[slicer, slicer] = source['source']
+# source_real = np.zeros((maxi*2,)*2)
+# source_real[slicer, slicer] = source['source']
+source_real = source['source']
 _, shiftx, shifty = get_shift(
     source_real,
     source_reconstruction[maxind[0]-maxi:maxind[0]+maxi,
@@ -236,7 +242,14 @@ source_reconstruction = np.roll(
                                   maxind[1]-maxi:maxind[1]+maxi],
             -shiftx, axis=0),
     -shifty, axis=1)
-source_reconstruction = source_reconstruction[slicer, slicer]
+
+# var = D(ift.full(D.domain, 1.))
+# source_var = HT(var).val
+# source_var = np.roll(
+#     np.roll(source_var[maxind[0]-maxi:maxind[0]+maxi,
+#                        maxind[1]-maxi:maxind[1]+maxi],
+#             -shiftx, axis=0),
+#     -shifty, axis=1)
 
 
 fig, axes = plt.subplots(2, 3, figsize=(19, 10))
@@ -245,24 +258,197 @@ ims = np.zeros_like(axes)
 # ims[0, 1] = axes[0, 1].imshow(HT(m).val.T, origin='lower')
 # ims[0, 2] = axes[0, 2].imshow(
 #     isource.val-HT(m).val.T, origin='lower', cmap='RdBu_r')
-ims[0, 0] = axes[0, 0].imshow(source['source'], origin='lower')
-ims[0, 1] = axes[0, 1].imshow(source_reconstruction, origin='lower')
+ims[0, 0] = axes[0, 0].imshow(source['source'], vmin=-0.1, vmax=1.0, origin='lower')
+ims[0, 1] = axes[0, 1].imshow(source_reconstruction, vmin=-0.1, vmax=1.0, origin='lower')
 ims[0, 2] = axes[0, 2].imshow(
-    source['source']-source_reconstruction, origin='lower', vmin=-0.1, vmax=0.4)# , cmap='RdBu_r')
+    source['source']-source_reconstruction, origin='lower', vmin=-0.3, vmax=0.3, cmap='RdBu_r')
 ims[1, 0] = axes[1, 0].imshow(d, **imargs, vmin=-0.10, vmax=0.8)
 ims[1, 1] = axes[1, 1].imshow(field, **imargs, vmin=-0.10, vmax=0.8)
 ims[1, 2] = axes[1, 2].imshow(
-    (d-field)/noise_scale, **imargs, cmap='gray', vmin=-3.0, vmax=3.0)
+    (d-field)/noise_scale, **imargs, cmap='RdBu_r', vmin=-3.0, vmax=3.0)
 axes[0, 0].set_title('source')
 axes[0, 1].set_title('rec')
 axes[0, 2].set_title('source - rec')
 axes[1, 0].set_title('data')
 axes[1, 1].set_title('BLs')
-axes[1, 2].set_title('data - BLs')
+axes[1, 2].set_title('(data - BLs)/noisescale')
 for im, ax in zip(ims.flatten(), axes.flatten()):
     plt.colorbar(im, ax=ax)
 plt.tight_layout()
 plt.show()
 
-# plt.savefig('output/smask_WienerFilter_n{}.png'.format(noise_scale))
-# plt.close()
+
+args = {
+    'offset_mean': 0,
+    'offset_std': (1e-3, 1e-6),
+    # Amplitude of field fluctuations
+    'fluctuations': (1., 1e-2),  # 1.0, 1e-2
+    # Exponent of power law power spectrum component
+    'loglogavgslope': (-6., 1),  # -6.0, 1
+    # Amplitude of integrated Wiener process power spectrum component
+    'flexibility': (1, .5),  # 1.0, 0.5
+    # How ragged the integrated Wiener process component is
+    'asperity': (0.1, 0.5)  # 0.1, 0.5
+}
+
+correlated_field = ift.exp(ift.SimpleCorrelatedField(isspace, **args))
+
+ic_sampling = ift.AbsDeltaEnergyController(deltaE=0.5, iteration_limit=500)
+ic_newton = ift.AbsDeltaEnergyController(name='Newton', deltaE=0.05, convergence_level=1, iteration_limit=10)
+minimizer = ift.NewtonCG(ic_newton)
+ic_sampling_nl = ift.AbsDeltaEnergyController(deltaE=0.5, iteration_limit=50, convergence_level=2)
+minimizer_sampling = ift.NewtonCG(ic_sampling_nl)
+
+N = ift.ScalingOperator(data_space, noise_scale**2, sampling_dtype=float)
+likelihood_energy = (
+    ift.GaussianEnergy(data=data, inverse_covariance=N.inverse) @ BB @ Re @ interpolator @ Trans @ correlated_field
+)
+H = ift.StandardHamiltonian(likelihood_energy, ic_sampling)
+initial_mean = ift.from_random(H.domain, 'normal')
+mean = initial_mean
+
+N_samples = 5
+# Draw new samples to approximate the KL six times
+for i in range(6):
+
+    if i >= 2:
+        if i > 5:
+            N_samples = 7
+        elif i == 9:
+            N_samples = 12
+
+        print(i, 'Samples:{}'.format(N_samples))
+
+        ic_newton = ift.AbsDeltaEnergyController(
+            name='Newton {}'.format(i), deltaE=1e-6, convergence_level=1, iteration_limit=20)
+        minimizer = ift.NewtonCG(ic_newton)
+        KL = ift.SampledKLEnergy(mean, H, N_samples, minimizer_sampling, mirror_samples=True)
+    else:
+        KL = ift.SampledKLEnergy(mean, H, N_samples, None, mirror_samples=True)
+
+    # Draw new samples and minimize KL
+    KL, conve = minimizer(KL)
+    mean = KL.position
+
+    nmean, var = KL.samples.sample_stat(correlated_field)
+
+    # rec = correlated_field(mean).val
+    rec = nmean.val
+    source_reconstruction = np.zeros_like(rec)
+    source_reconstruction[rec <= 10] = rec[rec < 120]
+    source_var = var.val
+
+    maxi = 120
+    # maxind = np.unravel_index(
+    #     np.argmax(source_reconstruction, axis=None),
+    #     source_reconstruction.shape
+    # )
+    slicer = slice(maxi//2, (maxi*3)//2)
+    # source_real = np.zeros((maxi*2,)*2)
+    # source_real[slicer, slicer] = source['source']
+    source_real = source['source']
+    _, shiftx, shifty = get_shift(
+        source_real,
+        source_reconstruction[maxind[0]-maxi:maxind[0]+maxi,
+                              maxind[1]-maxi:maxind[1]+maxi],
+        10
+    )
+    source_reconstruction = np.roll(
+        np.roll(source_reconstruction[maxind[0]-maxi:maxind[0]+maxi,
+                                      maxind[1]-maxi:maxind[1]+maxi],
+                -shiftx, axis=0),
+        -shifty, axis=1)
+    source_var = np.roll(
+        np.roll(source_var[maxind[0]-maxi:maxind[0]+maxi,
+                           maxind[1]-maxi:maxind[1]+maxi],
+                -shiftx, axis=0),
+        -shifty, axis=1)
+
+    field = BB(Re(interpolator(Trans(nmean)))).val
+
+    # fig, axes = plt.subplots(2, 3, figsize=(19, 10))
+    # ims = np.zeros_like(axes)
+    # ims[0, 0] = axes[0, 0].imshow(
+    #     source['source'], origin='lower', vmin=0.0, vmax=1.0)
+    # ims[0, 1] = axes[0, 1].imshow(
+    #     source_reconstruction, origin='lower', vmin=0.0, vmax=1.0)
+    # ims[0, 2] = axes[0, 2].imshow(
+    #     source['source']-source_reconstruction, origin='lower', cmap='RdBu_r', vmin=-0.3, vmax=0.3)
+    # ims[1, 0] = axes[1, 0].imshow(d, **imargs, vmin=-0.10, vmax=0.8)
+    # ims[1, 1] = axes[1, 1].imshow(field, **imargs, vmin=-0.10, vmax=0.8)
+    # ims[1, 2] = axes[1, 2].imshow((d-field)/noise_scale, **imargs, cmap='RdBu_r', vmin=-3, vmax=3)
+    # # axes[1, 2].contour(field > 0.1, **imargs, level=['white'])
+    # axes[0, 0].set_title('source')
+    # axes[0, 1].set_title('rec')
+    # axes[0, 2].set_title('source - rec')
+    # axes[1, 0].set_title('data')
+    # axes[1, 1].set_title('BLs')
+    # axes[1, 2].set_title('(data - BLs)/noisescale')
+    # for im, ax in zip(ims.flatten(), axes.flatten()):
+    #     plt.colorbar(im, ax=ax)
+    # plt.tight_layout()
+    # plt.show()
+
+np.save(
+    join('/home/jruestig/pro/python/lensing/',
+         source[dataname]['path'] + "_KLposition"),
+    KL.position.val,
+    allow_pickle=True
+)
+
+save_fits(
+    source_reconstruction,
+    join('/home/jruestig/pro/python/lensing/',
+         source[dataname]['path'] + "source.fits")
+)
+save_fits(
+    source_reconstruction,
+    join('/home/jruestig/pro/python/source_fwd/',
+         source['data_0.08_slacs']['path'],
+         'nonlinear_source.fits')
+)
+save_fits(
+    (d-field)/noise_scale,
+    join('/home/jruestig/pro/python/lensing/',
+         source[dataname]['path'] + "residual.fits")
+)
+save_fits(
+    Re(interpolator(Trans(nmean))).val,
+    join('/home/jruestig/pro/python/lensing/',
+         source[dataname]['path'] + "Ls.fits")
+)
+
+
+fig, axes = plt.subplots(2, 3, figsize=(3.5*4, 2*4))
+fig.suptitle('{} z{}_{}    ev={}    X^2={}'.format(
+    cluster,
+    source['z'],
+    source['id_source'],
+    0.0,
+    (((d-field)/noise_scale)**2).mean()))
+ims = np.zeros_like(axes)
+ims[0, 0] = axes[0, 0].imshow(
+    source['source'], origin='lower', vmin=0.0, vmax=1.0)
+ims[0, 1] = axes[0, 1].imshow(
+    source_reconstruction, origin='lower', vmin=0.0, vmax=1.0)
+ims[0, 2] = axes[0, 2].imshow(
+    source['source']-source_reconstruction, origin='lower', cmap='RdBu_r', vmin=-0.3, vmax=0.3)
+ims[1, 0] = axes[1, 0].imshow(d, **imargs, vmin=-0.10, vmax=0.8)
+ims[1, 1] = axes[1, 1].imshow(field, **imargs, vmin=-0.10, vmax=0.8)
+ims[1, 2] = axes[1, 2].imshow((d-field)/noise_scale, **imargs, cmap='RdBu_r', vmin=-3, vmax=3)
+# axes[1, 2].contour(field > 0.1, **imargs, level=['white'])
+axes[0, 0].set_title('source')
+axes[0, 1].set_title('rec')
+axes[0, 2].set_title('source - rec')
+axes[1, 0].set_title('data')
+axes[1, 1].set_title('BLs')
+axes[1, 2].set_title('(data - BLs)/noisescale')
+for im, ax in zip(ims.flatten(), axes.flatten()):
+    plt.colorbar(im, ax=ax)
+plt.tight_layout()
+plt.savefig(join(
+    '/home/jruestig/pro/python/source_fwd/output/New150sources/check_recs',
+    '{}_{}_{}_{}_{}.png'.format(
+        cluster, source['z'], source['id_source'], dataname, 'nonlinear')
+))
+plt.close()
